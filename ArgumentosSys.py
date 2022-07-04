7ad3116dd29758799f44f256c20bb3db0ab77225
Rodrigo de Souza <106630313+Rdg-93@users.noreply.github.com>
2022-07-04 00:41:13 -0300

    Add files via upload

---
+++ ArgumentosSys.py
@@ -0,0 +1,22 @@
+#!/usr/bin/
+
+import sys
+
+def argumentos(sys.args):
+    for argumentos in sys.args:
+    print (sys.args)
+
+print ("Hello World!")
+
+x = 5
+y = 10
+
+z = y + x
+
+print (z)
+
+soma = int(input('Digite o número para a soma:'))
+
+print (soma + z)
+
+argumentos()
